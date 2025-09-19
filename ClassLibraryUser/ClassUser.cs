c63138fd96ae63af6806ba8a16458309ec7614bf
THIAGO DE OLIVEIRA ROSA <thiago.orosa1@senacsp.edu.br>
2025-09-19 11:55:01 -0300

    feat(ClassLybraryUser): Add class User with Id,  Name, _Email and _Password, contrutor and methods Entrar() and Registrar() in the ClassLibraryUser project

---
+++ ClassLibraryUser/ClassUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryUser
+{
+    public class ClassUser
+    {
+        //Propriedade
+
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+
+        //Construtor
+        public ClassUser(int _id, string _name, string _email, string _password)
+        {
+            this.Id = _id;
+            this.Name = _name;
+            this.Email = _email;
+            this.Password = _password;
+        }
+
+        //Métodos
+        public string Entrar (string email, string password)
+        {
+            return "";
+        }
+
+        public string Registrar (string name, string email, string password)
+        {
+            return "";
+        }
+    }
+}
